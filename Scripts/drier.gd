218ca49bdaed032d473f575eb1bed68adbcb98c0
ZatzuStudent <20192681@s.ubaguio.edu>
2024-01-17 17:28:59 +0800

    fix
    fix

--- Scripts/drier.gd
+++ Scripts/drier.gd
@@ -1,12 +1,26 @@
 extends Area2D
 
-var full
+@onready var basket_contents_dried = $BasketContentsDried
 @onready var basket_contents = $BasketContents
+@onready var hasHerb = $HasHerb2D
+@onready var collision = $CollisionShape2D
+
 
 func _ready():
-	basket_contents = false
-	
-func _process(_delta):
-	if full == true:
+	basket_contents.visible = false
+	basket_contents_dried.visible = false
+	hasHerb.visible = false
+
+func _on_area_exited(area):
+	if area.is_in_group("herb1") && Input.is_action_just_released("mouse"):
+		$Timer.start()
 		basket_contents.visible = true
-		
+
+func _on_timer_timeout():
+	basket_contents_dried.visible = true
+	basket_contents.visible = false
+	hasHerb.visible = true
+
+func _on_has_herb_2d_area_exited(area):
+	if area.is_in_group("herb1"):
+		basket_contents_dried.visible = false
--- Scripts/herb_1.gd
+++ Scripts/herb_1.gd
@@ -12,8 +12,6 @@ var toDry = false
 @onready var button = $Button
 @onready var dry_timer = $"Dry'Timer"
 
-
-
 func _ready():
 	old_pos = position
 	gravity_scale = false
@@ -31,9 +29,7 @@ func _process(delta):
 		linear_velocity = (position-old_pos)/(delta*4)
 		old_pos = position
 	
-		
 func _on_dry_timer_timeout():
-	print('feds')
 	button.visible = true
 	dried.visible = true
 	fresh.visible = false
@@ -47,9 +43,13 @@ func _on_button_button_up():
 	_to_dry()
 		
 func _to_dry():
+	
 	if toDry == true && selected == false:
-		dry_timer.start(3)
-		print('GO')
+		global_position = Vector2(497,-80)
+		dry_timer.start(2)
+		global_position = Vector2(497,-80)
+		loveProperty = loveProperty*2
+		toDry = false
 		gravity_scale = false
 		button.visible = false
 		collision1.disabled = true
@@ -62,12 +62,8 @@ func _on_area_2d_area_entered(area):
 		queue_free()
 	if area.is_in_group("drier") && selected == true && fresh.visible == true:
 		toDry = true
+		
 	else:
 		return
 
 
-func _on_area_2d_area_exited(area):
-	if area.is_in_group("drier"):
-		toDry = false
-	else:
-		return
